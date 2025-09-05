9d326ec8c28e205779886f7a007e65dfa63dc5ac
baba530 <esg4649@gmail.com>
2025-09-05 19:09:58 +0300

    Update EsgModV7.lua

--- EsgModV7.lua
+++ EsgModV7.lua
@@ -1,119 +1,59 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 
 local Window = OrionLib:MakeWindow({
-	Name = "DeltaScripts - Blox Fruits GUI | Made by Esg 💀",
-	HidePremium = false,
-	SaveConfig = true,
-	ConfigFolder = "dxl_bf"
+    Name = "COPİLOT HUB | Reborn by Esg 💀",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "CopilotHub"
+})local FarmTab = Window:MakeTab({Name = "Farm", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local PvPTab = Window:MakeTab({Name = "PvP", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local FruitTab = Window:MakeTab({Name = "Fruit", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local MiscTab = Window:MakeTab({Name = "Misc", Icon = "rbxassetid://4483345998", PremiumOnly = false})FarmTab:AddButton({
+    Name = "Auto Farm Level",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EsgCopilot/scripts/main/autofarmlevel.lua"))()
+    end
 })
 
-local Tab = Window:MakeTab({
-	Name = "Scripts",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+FarmTab:AddButton({
+    Name = "Auto Farm Boss",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EsgCopilot/scripts/main/autofarmboss.lua"))()
+    end
+})FruitTab:AddButton({
+    Name = "Fruit Sniper",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EsgCopilot/scripts/main/fruitsniper.lua"))()
+    end
 })
 
-local Tab2 = Window:MakeTab({
-	Name = "Fruit Finder",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+FruitTab:AddButton({
+    Name = "Fruit ESP",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EsgCopilot/scripts/main/fruitesptracker.lua"))()
+    end
+})PvPTab:AddButton({
+    Name = "Camlock + Skill Spam",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EsgCopilot/scripts/main/pvpcombo.lua"))()
+    end
 })
 
-Tab2:AddButton({
-	Name = "Fruit Finder",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/AdminusGames/Blox_Fruits_Sniper/main/.lua'))()
-  	end    
+PvPTab:AddButton({
+    Name = "Anti Stun",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EsgCopilot/scripts/main/antistun.lua"))()
+    end
+})MiscTab:AddButton({
+    Name = "FPS Boost",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EsgCopilot/scripts/main/fpsboost.lua"))()
+    end
 })
 
-Tab:AddButton({
-	Name = "Domadic Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Domadicoof/Domadicoof/main/Domadichub/NottoGay/Start.ranscript"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "FTS X Hub!",
-	Callback = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/tufreescript/FTS-X-Hub/main/FTSXHUB%20V1'))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "T7GFY GUI",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/minded102/dokinbog/main/README.md", true))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "CFrame Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Steveee11/Scripts/main/Blox%20Fruits%20OP%20Script"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Flare Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/flares"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "BLCK Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/BLCK"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Modz Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/modz"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Hoho Hub",
-	Callback = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Lol Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/neon"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Zen Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader", true))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Uranium/Switch Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Augustzyzx/UraniumX/main/src_.lua.txt"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Thunder Z",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-05/HUB/main/Mobile-Loader"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Mukuro Hub",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua"))()
-  	end    
-})
+MiscTab:AddButton({
+    Name = "Anti-AFK",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EsgCopilot/scripts/main/antiafk.lua"))()
+    end
+})OrionLib:Init()
